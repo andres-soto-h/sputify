ffa1a8a8f19d4b9baf7e0b3d5327dfec4ec4167a
Andrés Soto <andres.soto.h@outlook.com>
2018-12-10 22:47:36 -0500

    Add restfull paths to app and creates views

--- app/controllers/artist_controller.rb
+++ app/controllers/artist_controller.rb
@@ -1,4 +1,41 @@
 class ArtistController < ApplicationController
+
   def index
+    @artist = Artist.all
   end
+
+  def show
+    @artist = Artist.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.create(product_params)
+    respond_to do |format|
+      format.html { redirect_to products_path }
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.update(params[:id], product_params)
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+  end
+
+  private
+
+  def artist_params
+    params.require(:artist).permit(:id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  get 'artist/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: "artist#index"
+
+  resources :artist
+
 end
