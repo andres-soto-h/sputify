41d370445ff03528f2920248120d70bdb0d384f1
Andrés Soto <andres.soto.h@outlook.com>
2018-12-10 22:47:49 -0500

    some artist data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Artist.create(name:"One Republic", image_url:"https://studiosol-a.akamaihd.net/uploadfile/letras/fotos/2/0/1/c/201c58f519fb50c6fdd0e553804f2af8.jpg")
+Artist.create(name:"Shawn Mendes", image_url:"https://los40es00.epimg.net/los40/imagenes/2018/02/14/musica/1518611568_189021_1518611899_noticia_normal.jpg")
+Artist.create(name:"Coldplay", image_url:"https://estaticos.elperiodico.com/resources/jpg/8/6/1488815675668.jpg")
+Artist.create(name:"All Time Low", image_url:"https://i2.wp.com/www.recovery-magazine.com/wp-content/uploads/2017/05/all_time_low_lj_190517.jpg?fit=1024%2C683")
+Artist.create(name:"GreenDay", image_url:"https://ksassets.timeincuk.net/wp/uploads/sites/55/2016/11/NME_GREENDAY_269_29482901_144821732.jpg")
